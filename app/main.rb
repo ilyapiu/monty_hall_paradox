bf3287c6297863ec1680a035c0d053bfba35f8b8
ilyapiu <ilya.piupau@gmail.com>
2022-09-16 11:03:54 +0300

    Else rewrite

--- app/main.rb
+++ app/main.rb
@@ -3,8 +3,7 @@
 require_relative 'monty_hall_problem'
 
 GAMES = ARGV[0].to_i
-CHANGE = ARGV[1].to_i
+WILL_CHANGE = ARGV[1].to_i
 
-monty = MontyHallPromblem.new
-arr = monty.itterate(GAMES, CHANGE)
-puts "Количество выигрышей #{arr[0]}, a количество проигрышей #{arr[1]}"
+games_result = MontyHallPromblem.new(GAMES, WILL_CHANGE).itterate()
+puts "Количество выигрышей #{games_result[:victories]}, a количество проигрышей #{games_result[:defeates]}"
--- app/monty_hall_problem.rb
+++ app/monty_hall_problem.rb
@@ -1,23 +1,29 @@
 # frozen_string_literal: true
 
 class MontyHallPromblem
+  attr_reader :itterations, :will_change
   RANGE = (1..3).freeze
 
-  def simulate(change)
+  def initialize(itterations, will_change)
+    @itterations = itterations
+    @will_change = will_change
+  end
+
+  def simulate()
+    win = 0
+    lose = 0
+    @itterations.times do
+      itterate() ? win += 1 : lose += 1
+    end
+    {victories: win, defeates: lose}
+  end
+  
+  private
+
+  def itterate()
     choice = rand(RANGE)
     car_door = rand(RANGE)
     empty_door = RANGE.to_a.select { |e| ![car_door, choice].include?(e) }.sample
-    choice = RANGE.to_a.select { |e| ![empty_door, choice].include?(e) }.sample if change == 1
+    choice = RANGE.to_a.select { |e| ![empty_door, choice].include?(e) }.sample if @will_change == 1
     choice == car_door
-  end
-
-  def itterate(itterations, change)
-    win = 0
-    lose = 0
-    itterations.times do
-      simulate(change) ? win += 1 : lose += 1
-    end
-
-    [win, lose]
-  end
 end
